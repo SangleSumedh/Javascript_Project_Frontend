1ffdf80d5a61ffdc66d8338e6145a0d584c6e011
Sumedh Sangle <sanglesumedh15@gmail.com>
2025-01-30 21:09:22 +0000

    Added local repo

---
+++ WeatherApp/script.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cityInput = document.getElementById("city-input");
+  const getWeatherBtn = document.getElementById("get-weather-btn");
+  const weatherInfo = document.getElementById("weather-info");
+  const cityNameDisplay = document.getElementById("city-name");
+  const temperatureDisplay = document.getElementById("temperature");
+  const descriptionDisplay = document.getElementById("description");
+  const errorMessage = document.getElementById("error-message");
+
+  const API_KEY = "3612bc118c998b622a19234964519616"; //env variables
+
+  getWeatherBtn.addEventListener("click", async () => {
+    const city = cityInput.value.trim();
+    if (!city) return;
+
+    try {
+      const weatherData = await fetchWeatherData(city);
+      displayWeatherData(weatherData);
+    } catch (e) {
+      showError();
+    }
+  });
+
+  async function fetchWeatherData(city) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
+
+    const response = await fetch(url);
+    console.log(typeof response);
+    console.log("Response: ", response);
+
+    if (!response.ok) {
+      throw new Error(" City Not Found");
+    }
+
+    const data = await response.json();
+    return data;
+  }
+
+  function displayWeatherData(weatherData) {
+    console.log(weatherData);
+    const { name, main, weather } = weatherData;
+    cityNameDisplay.textContent = name;
+    temperatureDisplay.textContent = `Temperature: ${main.temp}`;
+    descriptionDisplay.textContent = `Weather: ${weather[0].description}`;
+
+    //unlock the display
+    weatherInfo.classList.remove("hidden");
+    errorMessage.classList.add("hidden");
+  }
+
+  function showError() {
+    weatherInfo.classList.add("hidden");
+    errorMessage.classList.remove("hidden");
+  }
+});
